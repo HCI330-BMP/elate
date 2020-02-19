dcfb746bbde396378919e870554f9d3176130190
zb2450 <zaddeen@gmail.com>
2020-02-19 17:04:55 -0600

    please work

---
+++ login.js
@@ -0,0 +1,17 @@
+function validate = () => {
+  var username = document.getElementById('username').value;
+  var passsword = document.getElementById('password').value;
+  if ( username == "johnsmith1" && password == "password1") {
+    window.location = "user1.html";
+    return false;
+  }
+  else if ( username == "buymepants" && password == "password2") {
+    window.location = "user2.html";
+    return false;
+  }
+  else {
+    alert("Invalid credentials.");
+  }
+}
+
+document.querySelector('#login_button').onclick = validate();
