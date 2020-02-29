33d24240c6c8a89845c902fdc74ab3f35e11f605
Sanath Angalakudati <sangalakudati@gmail.com>
2020-02-29 15:17:02 -0600

    added rando background

--- js/menu.js
+++ js/menu.js
@@ -15,6 +15,31 @@ const loadHeader = () => {
   document.querySelector('header').innerHTML = template;
 }
 
+const randomBackground = () => {
+  console.log("Hello");
+  body = document.querySelector('body');
+
+  num = Math.random();
+  console.log(num);
+  if ( 0 < num < 0.2) {
+    body.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(images/nature_2.jpg)";
+  } else if(0.2 < num < 0.4) {
+    body.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(images/nature_3.jpg)";
+  } else if(0.4 < num < 0.6) {
+    body.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url(images/nature_9.jpg)";
+  } else if(0.6 < num < 0.8) {
+    body.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(images/nature_5.jpg)";
+  } else {
+    body.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(images/nature_7.jpg)";
+  }
+  body.style.height = 100;
+  body.style.backgroundPosition = 'center';
+  body.style.backgroundRepeat = 'no-repeat';
+  body.style.backgroundSize = 'cover';
+  body.style.position = 'relative';
+
+};
+
 const rotateBars = () => {
   btn = document.querySelector('.dropdownbtn');
   btn.style.transform = 'rotate(90deg)';
